2ed6b0b4c30057f02e1c719d19f65447496e56d9
shabeebdilna <shabeebdilna@gmail.com>
2024-01-17 14:45:04 +0530

    initial commit

---
+++ src/Components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Businuss from "./Businuss";
+import Client from "./Client";
+import Cta from "./Cta";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Businuss,
+    Client,
+    Cta,
+    Stats,
+    Footer,
+    Testimonial,
+    Hero,
+}
